f120637d7b5d4bfaa5f96db1ae17ccd599fa989d
Hackpython368 <prakashvidya368@gmail.com>
2025-11-10 12:42:13 +0530

    Completed announcement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import gtts
 def audioGenerator(lst):
     file_lst = []
     for i in range(len(lst)):
-        gtts.gTTS(text=lst[i],lang="hi").save(f"AudioFiles/GeneratedFiles/{i}.mp3")
+        gtts.gTTS(text=lst[i],lang="hi",slow=True).save(f"AudioFiles/GeneratedFiles/{i}.mp3")
         file_lst.append(f"AudioFiles/GeneratedFiles/{i}.mp3")
     return file_lst
 
@@ -20,7 +20,7 @@ def stichAnnouncement(lst):
     for i in pre_audio:
         load_pre.append(AudioSegment.from_mp3(f"AudioFiles/Pre-audioFiles/{i}"))
 
-    final_audio = load_pre[0] + load_pre[1] + read_audio[0] + read_audio[1] + load_pre[2] + read_audio[2] + load_pre[3] + read_audio[4] + load_pre[4]
+    final_audio = load_pre[0] + load_pre[1] + read_audio[0] + read_audio[1] + load_pre[2] + read_audio[2] + load_pre[3] + read_audio[3] + load_pre[4] + read_audio[4] +load_pre[5]
     final_audio._spawn(final_audio.raw_data,overrides={
         "frame_rate": int(final_audio.frame_rate * 0.8)
     })
